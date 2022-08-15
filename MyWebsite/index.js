ede302686fbdf666847b851e0d7ae8d3d41d2a80
Maxim <albinauric21@gmail.com>
2022-08-15 21:37:40 +0300

    More content

--- MyWebsite/index.js
+++ MyWebsite/index.js
@@ -5,13 +5,44 @@
 //     }, 1500);
 // });
 
-function PlayAnimation() {
-    var paper = document.getElementsByClassName("paperImage")[0];
+var paper;
+
+function PlayAnimationAbout() {
+    paper = GetPaper();
     paper.classList.add('animated');
     let timeout;
-    timeout = setTimeout(FinishAnim, 1500);
+    timeout = setTimeout(FinishAnimAbout, 1500);
 }
 
-function FinishAnim() {
+function PlayAnimationProjects() {
+    paper = GetPaper();
+    paper.classList.add('animated');
+    let timeout;
+    timeout = setTimeout(FinishAnimProjects, 1500);
+}
+
+function PlayAnimationBlog() {
+    paper = GetPaper();
+    paper.classList.add('animated');
+    let timeout;
+    timeout = setTimeout(FinishAnimBlog, 1500);
+}
+
+function FinishAnimAbout() {
+    paper.classList.remove('animated');
     location.href = "About/about.html";
+}
+
+function FinishAnimProjects() {
+    paper.classList.remove('animated');
+    location.href = "Projects/projects.html";
+}
+
+function FinishAnimBlog() {
+    paper.classList.remove('animated');
+    location.href = "Blog/blog.html";
+}
+
+function GetPaper() {
+    return document.getElementsByClassName("paperImage")[0];
 }
