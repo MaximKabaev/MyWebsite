46ce7ac028e1d4a1cdbaf791299cacefc3a66df9
MaximKabaev <mkabaev2020@gmail.com>
2022-08-19 23:03:11 +0300

    Update

--- MyWebsite/index.js
+++ MyWebsite/index.js
@@ -10,35 +10,54 @@ var paper;
 function PlayAnimationAbout() {
     paper = GetPaper();
     paper.classList.add('animated');
+
     let timeout;
-    timeout = setTimeout(FinishAnimAbout, 1500);
+    timeout = setTimeout(FinishAnimAbout, 1000);
 }
 
 function PlayAnimationProjects() {
     paper = GetPaper();
     paper.classList.add('animated');
     let timeout;
-    timeout = setTimeout(FinishAnimProjects, 1500);
+    timeout = setTimeout(FinishAnimProjects, 1000);
 }
 
 function PlayAnimationBlog() {
     paper = GetPaper();
     paper.classList.add('animated');
     let timeout;
-    timeout = setTimeout(FinishAnimBlog, 1500);
+    timeout = setTimeout(FinishAnimBlog, 1000);
 }
 
 function FinishAnimAbout() {
+    var contentBodyElement = document.getElementById("content-body");
+    contentBodyElement.style.display = "none";
+
+    var bodyElement = document.getElementById("body");
+    bodyElement.style.backgroundImage = "url('Pictures/paper.jpg')";
+
     paper.classList.remove('animated');
     location.href = "About/about.html";
 }
 
 function FinishAnimProjects() {
+    var contentBodyElement = document.getElementById("content-body");
+    contentBodyElement.style.display = "none";
+
+    var bodyElement = document.getElementById("body");
+    bodyElement.style.backgroundImage = "url('Pictures/paper.jpg')";
+
     paper.classList.remove('animated');
     location.href = "Projects/projects.html";
 }
 
 function FinishAnimBlog() {
+    var contentBodyElement = document.getElementById("content-body");
+    contentBodyElement.style.display = "none";
+
+    var bodyElement = document.getElementById("body");
+    bodyElement.style.backgroundImage = "url('Pictures/paper.jpg')";
+    
     paper.classList.remove('animated');
     location.href = "Blog/blog.html";
 }
@@ -46,3 +65,7 @@ function FinishAnimBlog() {
 function GetPaper() {
     return document.getElementsByClassName("paperImage")[0];
 }
+
+setTimeout(function(){
+    document.body.className="preload";
+},3500);
