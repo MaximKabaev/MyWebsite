85d03cf6624a1d093912e63c0014366f378adb13
MaximKabaev <mkabaev2020@gmail.com>
2022-08-22 23:36:27 +0300

    animations added

--- index.js
+++ index.js
@@ -13,6 +13,27 @@ contentBodyElement.style.display = "visible";
 var bodyElement = document.getElementById("body");
 bodyElement.style.backgroundImage = "url('Pictures/paper.jpg')";
 
+//Assigning/Removing class to navlink class
+var navLinks = document.querySelectorAll(".nav-link-parent");
+var count = 0;
+var lastCount = -1;
+
+setInterval(function() {
+    if(count != lastCount) {
+        navLinks[count].classList.add("animate__animated", "animate__backInUp"); //adding animation class to navlink
+        navLinks[count].classList.remove("invisible"); //removing invisible class from navlink
+        navLinks[count].style.setProperty('--animate-duration', '0.25s'); //adding animation duration to navlink
+        lastCount = count; //setting lastCount to count
+        navLinks[count].addEventListener('animationend', () => {
+            navLinks[count].classList.remove("animate__animated", "animate__backInUp");
+            count++;
+        }
+        );
+    }
+}
+, 5);
+
+
 var typing;
 
 var i = 0;
@@ -35,8 +56,6 @@ let stateCheck = setInterval(() => {
 
 //TYPING ANIMATION
 //
-
-
 function typeWriter() {
   if (i < txt.length) {
     typing.innerHTML += txt.charAt(i);
@@ -50,55 +69,40 @@ function typeWriter() {
 var paper;
 
 function PlayAnimationAbout() {
-    paper = GetPaper();
-    paper.classList.add('animated');
+    LeavingAnimation();
 
     let timeout;
     timeout = setTimeout(FinishAnimAbout, 1000);
 }
 
 function PlayAnimationProjects() {
-    paper = GetPaper();
-    paper.classList.add('animated');
+    LeavingAnimation();
     let timeout;
     timeout = setTimeout(FinishAnimProjects, 1000);
 }
 
 function PlayAnimationBlog() {
-    paper = GetPaper();
-    paper.classList.add('animated');
+    LeavingAnimation();
     let timeout;
     timeout = setTimeout(FinishAnimBlog, 1000);
 }
 
 function FinishAnimAbout() {
-    var contentBodyElement = document.getElementById("content-body");
-    contentBodyElement.style.display = "none";
-
-    var bodyElement = document.getElementById("body");
-    bodyElement.style.backgroundImage = "url('Pictures/paper.jpg')";
+    LeavingPage();
 
     paper.classList.remove('animated');
     location.href = "About/about.html";
 }
 
 function FinishAnimProjects() {
-    var contentBodyElement = document.getElementById("content-body");
-    contentBodyElement.style.display = "none";
-
-    var bodyElement = document.getElementById("body");
-    bodyElement.style.backgroundImage = "url('Pictures/paper.jpg')";
+    LeavingPage();
 
     paper.classList.remove('animated');
     location.href = "Projects/projects.html";
 }
 
 function FinishAnimBlog() {
-    var contentBodyElement = document.getElementById("content-body");
-    contentBodyElement.style.display = "none";
-
-    var bodyElement = document.getElementById("body");
-    bodyElement.style.backgroundImage = "url('Pictures/paper.jpg')";
+    LeavingPage();
     
     paper.classList.remove('animated');
     location.href = "Blog/blog.html";
@@ -106,6 +110,24 @@ function FinishAnimBlog() {
 
 function GetPaper() {
     return document.getElementsByClassName("paperImage")[0];
+}
+
+function LeavingAnimation() {
+    paper = GetPaper();
+    paper.classList.add('animated');
+    // var navLinks = document.querySelectorAll(".nav-link-parent");
+
+    // for(var i = 0; i < navLinks.length; i++){
+    //     navLinks[i].classList.add("animate__animated", "animate__backOutUp");
+    // }
+}
+
+function LeavingPage(){
+    var contentBodyElement = document.getElementById("content-body");
+    contentBodyElement.style.display = "none";
+
+    var bodyElement = document.getElementById("body");
+    bodyElement.style.backgroundImage = "url('Pictures/paper.jpg')";
 }
 
 setTimeout(function(){
