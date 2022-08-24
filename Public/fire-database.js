f5a477b2c8e11a958a1d1776b0c780176fe9e635
MaximKabaev <mkabaev2020@gmail.com>
2022-08-24 21:48:18 +0300

    comment section upgraded

--- Public/fire-database.js
+++ Public/fire-database.js
@@ -11,7 +11,8 @@ export function applyToFirebase(firebase, user, data, collectionName) {
     commentsRef.add({
         uid: user.uid,
         comment: data,
-        createdAt: serverTimestamp()
+        createdAt: serverTimestamp(),
+        name: user.displayName
     });
 }
 
@@ -28,8 +29,22 @@ export function readFirebase(firebase, collectionName, element){
 
             const items = querySnapshot.docs.map(doc => {
 
-                var t = `<li>${doc.data().comment}</li>`
-                if(t != '<li></li>'){
+                var t = 
+                `<li>
+                    <div class="comment-container">
+                        <div class="comment-author">
+                            <p>${doc.data().name}</p>
+                        </div>
+                        <div class="comment-text">
+                            <p>${doc.data().comment}</p>
+                        </div>
+                        <div class="comment-date">
+                            <p>${new Date(doc.data().createdAt.seconds * 1000).toDateString()}</p>
+                        </div>
+                    </div>
+                </li>
+                <br>`;
+                if(doc.data().comment != ''){
                     return t;
                 }
 
